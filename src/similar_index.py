378e265f577cf4b7df2292093d7c4579996b78c4
zhiyue <cszhiyue@gmail.com>
2016-10-21 05:27:11 +0800

    Index results are synchronized to the database

--- src/similar_index.py
+++ src/similar_index.py
@@ -3,12 +3,19 @@
 """
 
 """
+import argparse
 import logging
 from gensim.corpora import Dictionary
 from gensim.corpora import MmCorpus
 from preprocessing import TxtCorpus
 import os
 from gensim import corpora, models, similarities
+from sqlalchemy import create_engine
+from sqlalchemy.pool import SingletonThreadPool
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+from .spider.config import DB_URI, BOOK_INFO_INDEX_PER_PAGE_NUM
+from .database import NetBook, Category, Recommend
 
 __author__ = 'zhiyue'
 __copyright__ = "Copyright 2016"
@@ -20,6 +27,9 @@ __email__ = "cszhiyue@gmail.com"
 __status__ = "Production"
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+engine = create_engine(DB_URI, poolclass=SingletonThreadPool, pool_size=20)
+session_factory = sessionmaker(bind=engine)
 
 
 class TxtSimilar(object):
@@ -36,6 +46,35 @@ class TxtSimilar(object):
                                                      num_features=num_features,
                                                      num_best=num_best)
         return similarities_index
+
+    def update_index_to_db(self):
+        Session = scoped_session(session_factory)
+        session = Session()
+        for doc_index, similarities in enumerate(self.similarities_index):
+
+            similarities = sorted(similarities, key=lambda item: -item[1])[1:]
+            file_name = self.labels[doc_index]
+            book_info = session.query(NetBook).filter(NetBook.file_name == file_name).first()
+            range_index = 1
+            for sim_index, sim in similarities:
+                similar_book_info = session.query(NetBook).filter(NetBook.file_name == self.labels[sim_index]).first()
+                recommend_record = dict()
+                recommend_record['name'] = book_info.name
+                recommend_record['author'] = book_info.author
+                recommend_record["file_name"] = book_info.file_name
+                recommend_record['similar_book_name'] = similar_book_info.name
+                recommend_record['similar_file_name'] = similar_book_info.file_name
+                recommend_record['similar_book_author'] = similar_book_info.author
+                recommend_record['similar_book_wordcount'] = similar_book_info.word_count
+                recommend_record['similarity'] = sim
+                recommend_record['model'] = 'tfidf'
+                recommend_record['range'] = range_index
+                range_index += 1
+
+                new_recommend = Recommend(**recommend_record)
+                session.merge(new_recommend)
+                session.commit()
+        Session.remove()
 
     @staticmethod
     def load_index(path):
@@ -56,7 +95,7 @@ class TxtSimilar(object):
         dict_path = os.path.join(save_path, 'corpus.dict')
         mm_path = os.path.join(save_path, 'corpus.mm')
         labels_path = os.path.join(save_path, 'corpus.labels')
-        tfidf_module = os.path.join(save_path, 'corpus.tfidf')
+        tfidf_module = os.path.join(save_path, 'tfidf.model')
         corpus = MmCorpus(mm_path)
         dictionary = Dictionary.load(dict_path)
         labels = TxtCorpus.load_labels(labels_path)
@@ -67,17 +106,39 @@ class TxtSimilar(object):
 
 
 if __name__ == '__main__':
-    similarities_index = similarities.Similarity.load('data/similarities_index.0')
-    similarities_index.num_best = 20
-    labels = TxtCorpus.load_labels('data/corpus.labels')
-    with open("txt_index.txt", 'w') as fw:
-        for doc_index, similarities in enumerate(similarities_index):
-            similarities = sorted(similarities, key=lambda item: -item[1])[1:]
-            # print "similar to doc: %s" % labels[doc_index]
-            logging.info("%s / %s", doc_index, len(labels))
-            fw.write("similar to doc: %s" % labels[doc_index] + "\n")
-            for sim_index, sim in similarities:
-                # print labels[sim_index], str(round(sim*100, 3))+'%'
-                fw.write(labels[sim_index] + "," + str(round(sim * 100, 3)) + '% ' + "\n")
-            fw.write("\n")
-            fw.flush()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source", "-s", help="The path to the tfidf corpus")
+    parser.add_argument("--destination", "-d", help="The path to store the index")
+    args = parser.parse_args()
+    corpus_dir = args.source
+    index_dir = args.destination
+    if not os.path.isdir(corpus_dir):
+        os.mkdir(corpus_dir)
+    if not os.path.isdir(index_dir):
+        os.mkdir(index_dir)
+
+    logging.info("init TxtSimilar")
+    txt_similar = TxtSimilar(corpus_dir, index_path=index_dir)
+    logging.info("update index to database...")
+    txt_similar.update_index_to_db()
+    # similarities_index = txt_similar.similarities_index
+    # labels = txt_similar.labels
+
+    # corpus_tfidf = MmCorpus()
+    # logging.info("build similarities_index")
+    # similarities_index = similarities.Similarity('data/similarities_index', corpus_tfidf, num_features=len(dictionary), num_best=20)
+    # similarities_index = similarities.Similarity.load('data/similarities_index.0')
+    # labels = TxtCorpus.load_labels('data/corpus.labels')
+    # similarities_index.num_best = 20
+
+    # with open("txt_index.txt", 'w') as fw:
+    #     for doc_index, similarities in enumerate(similarities_index):
+    #         similarities = sorted(similarities, key=lambda item: -item[1])[1:]
+    #         # print "similar to doc: %s" % labels[doc_index]
+    #         logging.info("%s / %s", doc_index, len(labels))
+    #         fw.write("similar to doc: %s" % labels[doc_index] + "\n")
+    #         for sim_index, sim in similarities:
+    #             # print labels[sim_index], str(round(sim*100, 3))+'%'
+    #             fw.write(labels[sim_index] + "," + str(round(sim * 100, 3)) + '% ' + "\n")
+    #         fw.write("\n")
+    #         fw.flush()
