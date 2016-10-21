12506b7f278cc4327f8a0788f07745914cb94e74
zhiyue <cszhiyue@gmail.com>
2016-10-21 20:35:35 +0800

    Refactor the code

--- src/similar_index.py
+++ src/similar_index.py
@@ -16,7 +16,7 @@ from sqlalchemy.pool import SingletonThreadPool
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from src.spider.config import DB_URI, BOOK_INFO_INDEX_PER_PAGE_NUM
-from src.database import NetBook, Category, Recommend
+from src.models import NetBook, Category, Recommend
 
 __author__ = 'zhiyue'
 __copyright__ = "Copyright 2016"
@@ -33,32 +33,36 @@ engine = create_engine(DB_URI, poolclass=SingletonThreadPool, pool_size=20)
 session_factory = sessionmaker(bind=engine)
 
 
-class TxtSimilar(object):
-    def __init__(self, save_path, index_path='index/netbook', num_best=20):
-        self.corpus, self.dictionary, self.labels, self.tfidf = TxtSimilar._load(save_path)
-        if len(os.listdir(index_path)) > 0:
-            self.similarities_index = TxtSimilar.load_index(index_path)
-        else:
-            self.similarities_index = self.build_index(index_path, self.corpus, len(self.dictionary), num_best)
 
-    def build_index(self, index_path, corpus, num_features, num_best):
-        corpus_tfidf = self.tfidf[corpus]
-        similarities_index = similarities.Similarity(index_path, corpus_tfidf,
+
+class TxtSimilar(object):
+    def __init__(self, save_path):
+        self.corpus, self.dictionary, self.labels, self.tfidf = TxtSimilar._load(save_path)
+        self.similarities_index = dict()
+        self.register_models = dict()
+
+    @staticmethod
+    def build_index(index_path, corpus, num_features, model, num_best=21):
+        corpus_vec = model[corpus]
+        similarities_index = similarities.Similarity(index_path, corpus_vec,
                                                      num_features=num_features,
                                                      num_best=num_best)
         return similarities_index
 
-    def update_index_to_db(self):
+    def register_similariti_indexs(self, model_name, indexs):
+        self.similarities_index[model_name] = indexs
+
+    def update_index_to_db(self, model_name):
         Session = scoped_session(session_factory)
         session = Session()
-        for doc_index, similarities in enumerate(self.similarities_index):
+        for doc_index, similarities in enumerate(self.similarities_index[model_name]):
 
             similarities = sorted(similarities, key=lambda item: -item[1])[1:]
-            file_name = self.labels[doc_index].decode('utf8')
+            file_name = self.labels[doc_index]
             book_info = session.query(NetBook).filter(NetBook.file_name == file_name).first()
             range_index = 1
             for sim_index, sim in similarities:
-                similar_book_info = session.query(NetBook).filter(NetBook.file_name == self.labels[sim_index].decode('utf8')).first()
+                similar_book_info = session.query(NetBook).filter(NetBook.file_name == self.labels[sim_index]).first()
                 recommend_record = dict()
                 recommend_record['name'] = book_info.name
                 recommend_record['author'] = book_info.author
@@ -68,7 +72,7 @@ class TxtSimilar(object):
                 recommend_record['similar_book_author'] = similar_book_info.author
                 recommend_record['similar_book_wordcount'] = similar_book_info.word_count
                 recommend_record['similarity'] = sim
-                recommend_record['model'] = u'tfidf'
+                recommend_record['model'] = model_name
                 recommend_record['range'] = range_index
                 range_index += 1
 
@@ -77,38 +81,19 @@ class TxtSimilar(object):
                 session.commit()
         Session.remove()
 
-    def update_lsi_index(self, indexs=None):
-        Session = scoped_session(session_factory)
-        session = Session()
-        if indexs is None:
-            lsi = models.LsiModel(self.corpus, id2word=self.dictionary, num_topics=500)
-            lsi.show_topics(num_topics=100,num_words=20,log=True)
-            lsi.save("lsi.model")
-            indexs = Similarity("tfidf_index", lsi[self.corpus], num_features=500, num_best=20)
-        for doc_index, similarities in enumerate(indexs):
-            similarities = sorted(similarities, key=lambda item: -item[1])[1:]
-            file_name = self.labels[doc_index].decode('utf8')
-            book_info = session.query(NetBook).filter(NetBook.file_name == file_name).first()
-            range_index = 1
-            for sim_index, sim in similarities:
-                similar_book_info = session.query(NetBook).filter(NetBook.file_name == self.labels[sim_index].decode('utf8')).first()
-                recommend_record = dict()
-                recommend_record['name'] = book_info.name
-                recommend_record['author'] = book_info.author
-                recommend_record["file_name"] = book_info.file_name
-                recommend_record['similar_book_name'] = similar_book_info.name
-                recommend_record['similar_file_name'] = similar_book_info.file_name
-                recommend_record['similar_book_author'] = similar_book_info.author
-                recommend_record['similar_book_wordcount'] = similar_book_info.word_count
-                recommend_record['similarity'] = sim
-                recommend_record['model'] = u'lsi'
-                recommend_record['range'] = range_index
-                range_index += 1
+    def register_model(self, model_name, model):
+        self.register_models[model_name] = model
 
-                new_recommend = Recommend(**recommend_record)
-                session.merge(new_recommend)
-                session.commit()
-        Session.remove()
+    @staticmethod
+    def init_model(model, **kwargs):
+        return model(**kwargs)
+
+    def save_model(self, model_name, path):
+        self.register_models[model_name].save(path)
+
+    def load_model(self, model_name, model_type, path):
+        model = model_type.load(path)
+        self.register_models[model_name] = model
 
     @staticmethod
     def load_index(path):
@@ -150,30 +135,18 @@ if __name__ == '__main__':
         os.mkdir(corpus_dir)
     if not os.path.isdir(index_dir):
         os.mkdir(index_dir)
-
+    #index_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "data", "index"))
+    tfidf_index_path = os.path.join(index_dir, "tfidf_index")
     logging.info("init TxtSimilar")
-    txt_similar = TxtSimilar(corpus_dir, index_path=index_dir)
+    txt_similar = TxtSimilar(corpus_dir)
+    logging.info("init model.")
+    tfidf_model = TxtSimilar.init_model(models.TfidfModel, dictionary=txt_similar.dictionary)
+    logging.info("register model to models.")
+    txt_similar.register_model('tfidf', tfidf_model)
+    tfidf_similar_indexs = TxtSimilar.build_index(tfidf_index_path, txt_similar.corpus, len(txt_similar.dictionary), model=tfidf_model)
+    logging.info("register index to similarity indexs.")
+    txt_similar.register_similariti_indexs('tfidf', tfidf_similar_indexs)
     logging.info("update index to database...")
-    txt_similar.update_lsi_index()
+    txt_similar.update_index_to_db('tfidf')
+
     # txt_similar.update_index_to_db()
-    # similarities_index = txt_similar.similarities_index
-    # labels = txt_similar.labels
-
-    # corpus_tfidf = MmCorpus()
-    # logging.info("build similarities_index")
-    # similarities_index = similarities.Similarity('data/similarities_index', corpus_tfidf, num_features=len(dictionary), num_best=20)
-    # similarities_index = similarities.Similarity.load('data/similarities_index.0')
-    # labels = TxtCorpus.load_labels('data/corpus.labels')
-    # similarities_index.num_best = 20
-
-    # with open("txt_index.txt", 'w') as fw:
-    #     for doc_index, similarities in enumerate(similarities_index):
-    #         similarities = sorted(similarities, key=lambda item: -item[1])[1:]
-    #         # print "similar to doc: %s" % labels[doc_index]
-    #         logging.info("%s / %s", doc_index, len(labels))
-    #         fw.write("similar to doc: %s" % labels[doc_index] + "\n")
-    #         for sim_index, sim in similarities:
-    #             # print labels[sim_index], str(round(sim*100, 3))+'%'
-    #             fw.write(labels[sim_index] + "," + str(round(sim * 100, 3)) + '% ' + "\n")
-    #         fw.write("\n")
-    #         fw.flush()
